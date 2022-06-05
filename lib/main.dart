2450a3fb21606379e4aa24b24481d818a8eca718
lessdome <852604656@qq.com>
2022-06-05 17:02:51 +0800

    dashed_line 组件完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'widgets/star_rating.dart';
+import 'package:flutter_douban/views/mail/main.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -15,23 +14,7 @@ class MyApp extends StatelessWidget {
       title: "豆瓣",
       theme: ThemeData(
           primarySwatch: Colors.blue, splashColor: Colors.transparent),
-      home: const HYHomePage(),
-    );
-  }
-}
-
-class HYHomePage extends StatelessWidget {
-  const HYHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("豆瓣App")),
-      body: const Center(
-          child: StarRating(
-        rating: 11,
-        count: 5,
-      )),
+      home: const MainView(),
     );
   }
 }
